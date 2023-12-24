65975b8fadbb616dd3e98593778afa86b97b333d
Subhalakshmi S <ssubhassltce@gmail.com>
2023-12-24 13:28:51 +0530

    search box added

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,16 +1,31 @@
 import Product from "./Product";
+import { useState } from "react";
 
 const Products = ({ products, handleAddToCart, handleRemoveFromCart }) => {
+  const [searchText, setSearchText] = useState("");
   return (
-    <div className="row mt-4">
-      {products.map((product) => (
-        <Product
-          key={product.id}
-          product={product}
-          handleAddToCart={handleAddToCart}
-          handleRemoveFromCart={handleRemoveFromCart}
-        />
-      ))}
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        className="form-control"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <div className="row mt-4">
+        {products
+          .filter((product) =>
+            product.name.toLowerCase().includes(searchText.toLowerCase())
+          )
+          .map((product) => (
+            <Product
+              key={product.id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
+          ))}
+      </div>
     </div>
   );
 };
